220bed5c00c916a8f586ac60dcb4536eaa47acdd
wangqiming1001 <52316484+wangqiming1001@users.noreply.github.com>
2019-08-10 16:42:42 +0800

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -14,24 +14,29 @@ public class UserController {
     private UserMapper userMapper;
 
     @GetMapping("/user")
-    public List<User> findAll() {
-        return null;
+    public List<User> findAll(@RequestParam(defaultValue = "1") int pageNum,
+                              @RequestParam(defaultValue = "10") int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        return userMapper.findAllUser();
     }
 
     @GetMapping("/user/{userId}")
     public User findById(@PathVariable long userId) {
-        return null;
+        return userMapper.findByID(userId);
     }
 
     @PostMapping("/user")
     public void insertUser(@RequestBody User user) {
+        userMapper.addUser(user);
     }
 
     @PutMapping("/user/{userId}")
     public void updateUser(@RequestBody User user,@PathVariable int userId) {
+        userMapper.updateUser(user);
     }
 
     @DeleteMapping("/user/{userId}")
     public void deleteById(@PathVariable long userId) {
+        userMapper.deleteUser(userId);
     }
 }
