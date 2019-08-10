d1d2235bc5a796d89be7e6ede8b3b4e1cf398f29
wangqiming1001 <52316484+wangqiming1001@users.noreply.github.com>
2019-08-10 16:43:49 +0800

    Update UserMapper.java

--- src/main/java/com/example/demo/dao/UserMapper.java
+++ src/main/java/com/example/demo/dao/UserMapper.java
@@ -5,13 +5,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserMapper {
     // 1. 实现查询所有用户
-
+    @Select("select * from user")
+    List<User> findAllUser();
     // 2. 根据用户id查询用户
-
+    @Select("select * from user where id = #{id}")
+    User findByID(Integer id);
     // 3. 添加新用户
-
+    @Insert("insert into user(id, name) values(#{id}, #{name})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    Integer addUser(User user);
     // 4. 根据用户id删除用户
-
+    @Delete("delete from user where id = #{id}")
+    Integer deleteUser(Integer id);
     // 5. 根据用户id更新用户
-
+    @Update("update user set id = #{id}, name = #{name} where id = #{id}")
+    Integer updateUser(User user);
 }
